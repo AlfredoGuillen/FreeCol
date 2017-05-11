26154756cb36caefb69d1409b38489913d1c633e
ashinster <ashin4@students.towson.edu>
2017-05-11 19:04:39 -0400

    Fixed repeated conditional statement. Now checks if player and other variables are null, instead of checking if player was null twice. Bug found with FindBugs tool and was labeled as a 'Scary'

--- src/net/sf/freecol/client/control/InGameController.java
+++ src/net/sf/freecol/client/control/InGameController.java
@@ -3421,8 +3421,8 @@ public final class InGameController implements NetworkConstants {
      */
     public boolean firstContact(Player player, Player other, Tile tile,
                                 boolean result) {
-        if (player == null || player == null || player == other
-            || tile == null) return false;
+        if (player == null || other == null || player == other || tile == null)
+        	return false;
 
         boolean ret = askServer().firstContact(player, other, tile, result);
         if (ret) {
